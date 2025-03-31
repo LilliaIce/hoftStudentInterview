a0e8d435c86c777c6ecf900f7c91c2d23375cf84
Lillia Ice <lillia.ann.ice@gmail.com>
2025-03-31 10:16:27 -0500

    Updated video recorder to display the live video stream when a video isn't available. Otherwise, it'll display the finished video for review. Upon moving to the next question, the video will be saved and the variable will be cleared.

--- src/VideoRecorder.jsx
+++ src/VideoRecorder.jsx
@@ -8,32 +8,32 @@ export default function VideoRecorder({handleSubmit, total}) {
   const [recordedVideo, setRecordedVideo] = useState(null)
   const mediaRecorder = useRef(null)
   const liveVideoFeed = useRef(null)
-  const mimeType = "video/webm; codecs=\"opus,vp8\"";
+  const mimeType = "video/webm";
 
   const getCameraPermission = async () => {
     setRecordedVideo(null)
     if ("MediaRecorder" in window) {
         try {
-            const videoConstraints = {
-                audio: false,
-                video: {
-                  width: {min: 100, ideal: 320},
-                  height: {min: 100, ideal: 240},
-                  frameRate: {min: 10, ideal: 10}
-                }
+          const audioConstraints = {audio: true}
+          const videoConstraints = {
+            audio: false,
+            video: {
+              width: {min: 100, ideal: 320},
+              height: {min: 100, ideal: 240},
+              frameRate: {min: 10, ideal: 10}
             }
-            const audioConstraints = {audio: true}
-            const audioStream = await navigator.mediaDevices.getUserMedia(audioConstraints)
-            const videoStream = await navigator.mediaDevices.getUserMedia(videoConstraints)
-            setPermission(true)
-            const combinedStream = new MediaStream([
-                ...videoStream.getVideoTracks(),
-                ...audioStream.getAudioTracks()
-            ])
-            setStream(combinedStream)
-            liveVideoFeed.current.srcObject = videoStream
+          }
+          const audioStream = await navigator.mediaDevices.getUserMedia(audioConstraints)
+          const videoStream = await navigator.mediaDevices.getUserMedia(videoConstraints)
+          setPermission(true)
+          const combinedStream = new MediaStream([
+            ...videoStream.getVideoTracks(),
+            ...audioStream.getAudioTracks()
+          ])
+          setStream(combinedStream)
+          liveVideoFeed.current.srcObject = videoStream
         } catch (err) {
-            alert(err.message)
+          alert(err.message)
         }
     } else {
         alert("The MediaRecorder API is not supported in your browser.")
@@ -47,32 +47,32 @@ export default function VideoRecorder({handleSubmit, total}) {
     mediaRecorder.current.start()
     let localVideoChunks = []
     mediaRecorder.current.ondataavailable = (event) => {
-        if (typeof event.data === "undefined") return
-        if (event.data.size === 0) return
-        localVideoChunks.push(event.data)
+      if (typeof event.data === "undefined") return
+      if (event.data.size === 0) return
+      localVideoChunks.push(event.data)
     }
     setVideoChunks(localVideoChunks)
   }
 
   const stopRecording = () => {
-    setRecordingStatus("finished")
+    setRecordingStatus("inactive")
     mediaRecorder.current.stop()
     mediaRecorder.current.onstop = () => {
-        const videoBlob = new Blob(videoChunks, {type: mimeType})
-        const videoUrl = URL.createObjectURL(videoBlob)
-        setRecordedVideo(videoUrl)
-        setVideoChunks([])
+      const videoBlob = new Blob(videoChunks, {type: mimeType})
+      const videoUrl = URL.createObjectURL(videoBlob)
+      setRecordedVideo(videoUrl)
+      setVideoChunks([])
     }
   }
 
   return (
     <>
 			<div className="videoDiv">
-				{recordingStatus != "finished" ? (
-					<video ref={liveVideoFeed} autoPlay className="video"/>
-				) : (
+        {recordedVideo && recordingStatus == "inactive" ? (
 						<video className="video" src={recordedVideo} controls/>
-				)}
+				) : (
+          <video ref={liveVideoFeed} autoPlay className="video"/>
+        )}
 			</div>
       <div className="submit">
         {!permission ? (
@@ -90,14 +90,14 @@ export default function VideoRecorder({handleSubmit, total}) {
             Stop recording and upload answer
           </button>
         ) : ( null )}
-        {recordingStatus != "finished" ? (
+        {recordingStatus != "inactive" ? (
           <button type="button" id="noSubmit">
             Next question
           </button>
         ) : (
           <button type="button" onClick={() => {
             handleSubmit(total, recordedVideo)
-            setRecordingStatus("inactive")
+            setRecordedVideo(null)
           }}>
             Next question
           </button>
