8d58fe4b91eb13b5e588526e0baa7078b09862e0
Lillia Ice <lillia.ann.ice@gmail.com>
2025-04-02 13:35:54 -0500

    Added a Footer component. When the img tag is forced to confine to a width (preferably 50%), the footer hovers above the bottom of the page. When the img tag is allowed to expand, the footer sits flush to the bottom but the page has a scroll bar. In both cases, if the site is opened in fullscreen, the footer noticeably doesn't cross the entire width.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Intro from './Intro.jsx'
 import Header from './Header.jsx'
 import Questions from './Questions.jsx'
+import Footer from './Footer.jsx'
 
 export default function App() {
   const [testState, setTestState] = useState("starting")
@@ -15,40 +16,59 @@ export default function App() {
   if (testState == "starting") {
     return (
       <>
-        <Header/>
-        <Intro
-          setTestState={setTestState}
-          setLevel={setLevel}
-          firstName={firstName}
-          lastName={lastName}
-          emailAddress={emailAddress}
-        />
+        <div className="header">
+          <Header/>
+        </div>
+        <div className="bodyContent">
+          <Intro
+            setTestState={setTestState}
+            setLevel={setLevel}
+            firstName={firstName}
+            lastName={lastName}
+            emailAddress={emailAddress}
+          />
+        </div>
+        <div className="footer">
+          <Footer/>
+        </div>
       </>
     )
   }
   else if (testState == "in progress") {
     return (
       <>
-        <Header/>
-        <Questions
-          count={count}
-          setTestState={setTestState}
-          setCount={setCount}
-          level={level}
-          firstName={firstName}
-          lastName={lastName}
-          email={emailAddress}
-        />
+        <div className="header">
+          <Header/>
+        </div>
+        <div className="bodyContent">
+          <Questions
+            count={count}
+            setTestState={setTestState}
+            setCount={setCount}
+            level={level}
+            firstName={firstName}
+            lastName={lastName}
+            email={emailAddress}
+          />
+        </div>
+        <div className="footer">
+          <Footer/>
+        </div>
       </>
     )
   }
   else {
     return (
       <>
-        <Header/>
-        <p>
+        <div className="header">
+          <Header/>
+        </div>
+        <p className="bodyContent">
           Your test has been submitted
         </p>
+        <div className="footer">
+          <Footer/>
+        </div>
       </>
     )
   }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function Footer() {
+  return (
+    <>
+      <p>
+       © 2025 HOFT Institute – All rights reserved
+      </p>
+    </>
+  )
+}
